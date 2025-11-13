a95c5197f57ae16a1ce1c7ac8332ec126f5090a7
Danil Volodchenko <danil.volodchenko@inbox.ru>
2025-11-13 23:04:21 +0700

    adding new funcs

--- commands.py
+++ commands.py
@@ -1,3 +1,4 @@
+from typing import NoReturn
 import threading
 from collections.abc import Callable
 
@@ -17,10 +18,14 @@ class InitCommand(ICommand):
             return
 
         with self._lock:
-            self.scope['IoC.Scope.Set'] = lambda: SetLocalScopeCommand()
+            self.scope['IoC.Scope.Empty'] = lambda: {}
+            self.scope['IoC.Scope.Create'] = lambda *args: self.create_scope(args)
+            self.scope['IoC.Scope.Parent'] = lambda *args: self.parent_scope()
+            self.scope['IoC.Scope.Set'] = lambda *args: SetScopeCommand(args[0])
+            self.scope['IoC.Scope.Current'] = lambda *args: SetScopeCommand(args[0])
             self.scope['IoC.Register'] = lambda *args: RegisterDependencyCommand(args[0], args[1])
-            self.scope['IoC.Scopes.Scope'] = lambda: self.scope
-            self.scope['IoC.Scopes.LocalScope'] = lambda: self.local_scope
+            self.scope['IoC.Scopes.Scope'] = lambda *args: self.scope
+            self.scope['IoC.Scopes.LocalScope'] = lambda *args: self.local_scope
             self.scope['IoC.Scopes.LocalScope.Empty'] = lambda: EmptyLocalScopeCommand()
 
             IoC[ICommand].resolve(
@@ -28,7 +33,27 @@ class InitCommand(ICommand):
             ).execute()
 
             print(IoC.strategy)
-        self._is_executed = True
+            self._is_executed = True
+
+    def create_scope(self, *args) -> dict:
+        new_scope = IoC[dict].resolve('IoC.Scope.Empty')
+
+        if args:
+            parent_scope = args[0]
+        else:
+            parent_scope = IoC[dict].resolve('IoC.Scope.Current')
+        new_scope['IoC.Scope.Parent'] = parent_scope
+
+        return new_scope
+
+    def get_parent_scope(self) -> NoReturn:
+        raise ValueError('Root scope has not parent scope')
+
+    def get_current_scope(self):
+        try:
+            return IoC[threading.local].resolve('IoC.Scopes.LocalScope').value
+        except AttributeError:
+            return IoC[dict].resolve('IoC.Scopes.Scope')
 
 
 class RegisterDependencyCommand(ICommand):
@@ -47,13 +72,21 @@ class RegisterDependencyCommand(ICommand):
             scope[self.dependency] = self.strategy
 
 
-class SetLocalScopeCommand(ICommand):
+class SetScopeCommand(ICommand):
+    def __init__(self, scope: dict) -> None:
+        self.scope = scope
+
     def execute(self) -> None:
         local_scope = IoC[threading.local].resolve('IoC.Scopes.LocalScope')
-        local_scope.value = threading.get_ident()
+        local_scope.value = self.scope
 
 
 class EmptyLocalScopeCommand(ICommand):
     def execute(self) -> None:
         local_scope = IoC[threading.local].resolve('IoC.Scopes.LocalScope')
         del local_scope.value
+
+
+# нужно чтобы в local_scope (лучше в current_scope) находились текущие зависимости и ссылка на родительский scope
+# а в самом scope все зависимости который регаются при запуске InitCommand.execute()
+# и когда приходится искать зависимости, то по цепочке проходить текущий и вложенные скопы
